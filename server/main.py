01ccd13a7a47d3c637237a9e003fc60c293a9c6d
TUSHAR MISHRA <mishratushar8890@gmail.com>
2024-07-05 15:23:05 +0530

    ---

--- server/main.py
+++ server/main.py
@@ -1,3 +1,23 @@
 # fastapi dev main.py
 #.\venv\Scripts\activate
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class UserCreate(BaseModel):
+  name: str
+  email: str
+  password: str
+
+@app.post('/signup')
+def signup_user(user: UserCreate):
+  print(user.name)
+  print(user.email)
+  print(user.password)
+  
+  pass
+
+
+# 1.16
