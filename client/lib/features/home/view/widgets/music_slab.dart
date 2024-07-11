75590fdb5a617b439efe130d14ed31c54a0169cc
TUSHAR MISHRA <mishratushar8890@gmail.com>
2024-07-11 16:12:31 +0530

    song upload backend  done

---
+++ client/lib/features/home/view/widgets/music_slab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MusicSlab extends StatelessWidget {
+  const MusicSlab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- client/lib/main.dart
+++ client/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:sur/core/provider/current_user_notifier.dart';
 import 'package:sur/core/theme/theme.dart';
 import 'package:sur/features/auth/view/pages/signup_page.dart';
 import 'package:sur/features/auth/viewmodel/auth_viewmodel.dart';
-import 'package:sur/features/home/view/pages/upload_song.dart';
+import 'package:sur/features/home/view/pages/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,7 +30,7 @@ class MyApp extends ConsumerWidget {
       debugShowCheckedModeBanner: false,
       title: 'SUR',
       theme: AppTheme.darkThemeMode,
-      home: currentUser == null ? const SignupPage() : const UploadSongPage(),
+      home: currentUser == null ? const SignupPage() : const HomePage(),
     );
   }
 }
